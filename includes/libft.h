e639fbf4dffcaa972a186a6be1b424bb4ffe3319
Sorannian <m.dupuy780@laposte.net>
2016-11-20 07:40:43 +0100

    Manque memmove-memcmp-memchr

--- includes/libft.h
+++ includes/libft.h
@@ -8,6 +8,7 @@
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memccpy(void *dest, const void *src, int c, size_t n);
+void	*ft_memmove(void *dest, const void *src, size_t n);
 void	ft_bzero(void *s, size_t n);
 int	ft_strcmp(const char *s1, const char *s2);
 int	ft_strncmp(const char *s1, const char *s2, size_t n);
